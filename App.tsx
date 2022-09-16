3cb1eeb5fea0db10f72a40f3abec586207e53858
correa0inaiara <correa0inaiara@gmail.com>
2022-09-16 13:43:21 -0300

    Setup e código inicial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>
+        Hello React Native!
+      </Text>
+      <Button title="Send 1" />
+      <Button title="Send 2" />
+      <Button title="Send 3" />
+      <Button title="Send 4" />
+      <Button title="Send 5" />
       <StatusBar style="auto" />
     </View>
   );
 }
 
+interface ButtonProps {
+  title: string;
+}
+
+function Button (props: ButtonProps) {
+  return (
+    <TouchableOpacity 
+      style={styles.button}>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#fff',
+    fontSize: 22
+  },
+  button: {
+    margin: 10,
+    padding: 10,
+    borderWidth: 2,
+    borderColor: '#3e32a8',
+    backgroundColor: '#fff',
+    color: '#000',
+    cursor: 'pointer'
+  }
 });
